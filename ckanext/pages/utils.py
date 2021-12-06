a1e452e32e0a6a11ee7b3fb2209c26883bb917cd
Bart Saelen <bart.saelen@geosolutions.be>
2021-12-06 16:05:09 +0100

    fix bug

--- ckanext/pages/utils.py
+++ ckanext/pages/utils.py
@@ -62,9 +62,9 @@ def pages_edit(page=None, data=None, errors=None, error_summary=None, page_type=
             data_dict={'org_id': None, 'page': page}
         )
 
-        _parents = tk.get_action('ckanext_menu_list')(
-            data_dict={'parent_name': 'about'}
-        )
+    _parents = tk.get_action('ckanext_menu_list')(
+        data_dict={'parent_name': 'about'}
+    )
 
     if page_dict is None:
         page_dict = {}
