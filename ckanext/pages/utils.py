455d65fcf80294f224c61bf71662c05f28cba81a
Bart Saelen <bart.saelen@geosolutions.be>
2021-12-06 18:03:00 +0100

    integrate news page

--- ckanext/pages/utils.py
+++ ckanext/pages/utils.py
@@ -79,6 +79,10 @@ def pages_edit(page=None, data=None, errors=None, error_summary=None, page_type=
     if tk.request.method == 'POST' and not data:
         data = _parse_form_data(tk.request)
 
+        print("#" * 25)
+        print(data)
+        print("#" * 25)
+
         if not (data['name'] == 'about') and not (data['name'] == 'news') and (data['parent_name'] == ''):
             data['parent_name'] = 'about'
 
