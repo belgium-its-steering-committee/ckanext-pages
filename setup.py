690a6c7ad6b01a2907d0a8c5d57a93e8d8038516
Bart Saelen <bart.saelen@geosolutions.be>
2022-04-07 09:17:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ckanext-pages',
-    version='2.0.0',
+    version='2.1.1',
     description='Basic CMS extension for CKAN (Belgian ITS fork)',
     long_description='',
     classifiers=[
