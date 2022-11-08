e3246a1168f532a59f8d5798c0da49234c63324a
Felten Vanballenberghe <felten.vanballenberghe@geosolutions.be>
2022-11-08 18:18:00 +0100

    bump

--- ckanext/pages/assets/vendor/ckeditor/plugins/collapsibleItem/plugin.js
+++ ckanext/pages/assets/vendor/ckeditor/plugins/collapsibleItem/plugin.js
@@ -3,7 +3,7 @@ function getCollapsibleItem() {
         '<div class="panel panel-default collapsible-item" id="collapbsible-1">' +
         '   <div class="panel-heading collapsible-item-heading" role="tab" id="headingCollapbsible">' +
         '       <h4 class="panel-title collapsible-item-title">' +
-        '           <a class="collapsed collapsible-item-title-link-icon pull-right" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseCollapbsible" aria-expanded="false" aria-controls="collapseCollapbsible"><i class="fa fa-arrow-down"></i></a>' +
+        '           <a class="collapsed collapsible-item-title-link-icon pull-right" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseCollapbsible" aria-expanded="false" aria-controls="collapseCollapbsible">span><i class="fa fa-arrow-down"></i>&nbsp;</span></a>' +
         '           <a data-info="test div" class="collapsed collapsible-item-title-link" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseCollapbsible" aria-expanded="false" aria-controls="collapseCollapbsible"><span class="collapsible-item-title-link-text">Title Text</span></a>' +
         '       </h4>' +
         '   </div>' +
