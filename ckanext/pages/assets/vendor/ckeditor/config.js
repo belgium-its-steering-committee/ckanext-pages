5e1de5aac55460b65a8bb7941957626610366006
Felten Vanballenberghe <felten.vanballenberghe@geosolutions.be>
2022-11-08 22:03:44 +0100

    bump

--- ckanext/pages/assets/vendor/ckeditor/config.js
+++ ckanext/pages/assets/vendor/ckeditor/config.js
@@ -31,7 +31,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	config.format_tags = 'p;h1;h2;h3;pre;i';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
@@ -39,4 +39,6 @@ CKEDITOR.editorConfig = function( config ) {
 	//extra plugins
 	config.Extraplugins ='collapsibleItem';
 
+	//allow ALL content
+	config.allowedContent = true;
 };
