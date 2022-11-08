382ef5e37e4560fd95ff1c1c8ce80355e625e4bb
Felten Vanballenberghe <felten.vanballenberghe@geosolutions.be>
2022-11-09 00:07:05 +0100

    bump

--- ckanext/pages/assets/vendor/ckeditor/plugins/collapsibleItem/plugin.js
+++ ckanext/pages/assets/vendor/ckeditor/plugins/collapsibleItem/plugin.js
@@ -3,7 +3,7 @@ function getCollapsibleItem() {
         '<div class="panel panel-default collapsible-item" id="collapbsible-1">' +
         '   <div class="panel-heading collapsible-item-heading" role="tab" id="headingCollapbsible">' +
         '       <h4 class="panel-title collapsible-item-title">' +
-        '           <a data-info="test div"class="collapsed collapsible-item-title-link-icon pull-right" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseCollapbsible" aria-expanded="false" aria-controls="collapseCollapbsible"><span class="fa fa-arrow-down">&nbsp;</span></a>' +
+        '           <a class="collapsed collapsible-item-title-link-icon pull-right" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseCollapbsible" aria-expanded="false" aria-controls="collapseCollapbsible"><span class="fa fa-arrow-down">&nbsp;</span></a>' +
         '           <a class="collapsed collapsible-item-title-link" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseCollapbsible" aria-expanded="false" aria-controls="collapseCollapbsible"><span class="collapsible-item-title-link-text">Title Text</span></a>' +
         '       </h4>' +
         '   </div>' +
@@ -31,7 +31,7 @@ CKEDITOR.plugins.add('collapsibleItem', {
                 },
                 content: {
                     selector: '.collapsible-item-body',
-                    allowedContent: 'p;h1;h2;h3;h4;br;span(*)[*]ul;ol;li;strong;em;u;table(*)[*];tbody;thead;tr;td;th;hr;a;a[*];a(*)[*];img(*)[*];*{color};*{size}'
+                    allowedContent: 'p[*];h1[*];h2[*];h3[*];h4[*];br[*];span(*)[*];ul[*];ol[*];li[*];strong[*];em[*];u[*];table(*)[*];tbody[*];thead];tr[*];td[*];th[*];hr;a;a[*];a(*)[*];img(*)[*];'
                 }
             },
             allowedContent: 'div(!collapsible-item*,panel*,collapse)[*];h4(!collapsible-item*,panel*)[*];a(!collapsible-item*,collapsed,panel*)[*];span(*)[*];',
