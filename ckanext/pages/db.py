2fb411d32117cb948fc67e101ebb69a50dd34abc
felten onThePenguinMachine <felten@thePenguin.be>
2024-02-09 16:03:54 +0100

    08_db.py RowProxy row

--- ckanext/pages/db.py
+++ ckanext/pages/db.py
@@ -1,13 +1,18 @@
 import datetime
 import uuid
 import json
+
 from six import text_type
 import sqlalchemy as sa
 from sqlalchemy.orm import class_mapper
+
 try:
-    from sqlalchemy.engine.result import RowProxy as Row
+    from sqlalchemy.engine import row
 except ImportError:
-    from sqlalchemy.engine.base import RowProxy as Row
+    try:
+        from sqlalchemy.engine.result import RowProxy as Row
+    except ImportError:
+        from sqlalchemy.engine.base import RowProxy as Row
 
 from ckan import model
 from ckan.model.domain_object import DomainObject
