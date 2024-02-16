8c44f4e27dc433d8b294df5db40e86bc2b98f587
felten onThePenguinMachine <felten@thePenguin.be>
2024-02-16 15:56:58 +0100

    07_plugin.py get_plus_icon

--- ckanext/pages/plugin.py
+++ ckanext/pages/plugin.py
@@ -89,12 +89,17 @@ def get_recent_blog_posts(number=5, exclude=None):
 
     return new_list
 
-'''
+
 def get_plus_icon():
+    '''
     if tk.check_ckan_version(min_version='2.7'):
-        return 'plus-square'
-    return 'plus-sign-alt'
-'''
+    '''
+    return 'plus-square'
+    '''
+     return 'plus-sign-alt'
+    '''
+
+
 def pages_page_title(selected_lang, page_data):
     if selected_lang:
         if selected_lang == "nl" and page_data.get("title_nl", False):
